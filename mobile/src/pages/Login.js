a512e6eb6064fe95c4937ef2609273442c8fc12f
Matheus Mattana <matheusmattana@hotmail.com>
2019-10-03 17:53:33 -0300

    SpotList start

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { View, Image, Text, TextInput, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage, KeyboardAvoidingView, Platform, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png'
 
 
-export default function Login(){
+export default function Login({ navigation }){
+
+  const [email,setEmail] = useState('');
+  const [ techs, setTechs ] = useState('');
+
+  useEffect(()=>{
+    AsyncStorage.getItem('user').then(user => {
+      if (user){
+        navigation.navigate('List');
+      }
+    })
+  }, []);
+
+  async function handleSubmit(){
+    const response = await api.post('/sessions', {
+      email
+    })
+
+    const { _id } = response.data;
+
+    await AsyncStorage.setItem('user', _id);
+    await AsyncStorage.setItem('techs', techs);
+
+    navigation.navigate('List');
+  }
+
   return ( 
-  <View style={ styles.container }>
+  <KeyboardAvoidingView behavior='padding' style={ styles.container }>
     <Image source={logo} />
     <View style={styles.form}>
       <Text style={styles.label}>SEU-EMAIL *</Text>
@@ -17,6 +45,8 @@ export default function Login(){
         keyboardType="email-address"
         autoCapitalize="none"
         autoCorrect={false}
+        value={ email }
+        onChangeText={ setEmail }
       />
 
       <Text style={styles.label}>TECNOLOGIAS *</Text>
@@ -25,10 +55,16 @@ export default function Login(){
         placeholder="Tecnologias de interesse *"
         placeholderTextColor='#999'
         autoCapitalize="words"
-        autoCorrect={false}
+        autoCorrect={ false }
+        value={ techs }
+        onChangeText={ setTechs } 
       />
+
+      <TouchableOpacity onPress={ handleSubmit } style={ styles.button }>
+        <Text style={ styles.buttonText }>Encontrar spots</Text>
+      </TouchableOpacity>
     </View>
-  </View>
+  </KeyboardAvoidingView>
   );
 }
 
@@ -61,5 +97,19 @@ const styles = StyleSheet.create({
     marginBottom: 20, 
     borderRadius: 2
   },
+
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
 
 })
